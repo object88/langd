abc9ada71d50b6fdcadc093df0351208429d5d2f
object88 <object88@gmail.com>
2017-09-19 17:41:16 -0600

    Code cleanup

--- requests/didClose.go
+++ requests/didClose.go
@@ -64,32 +64,3 @@ func (rh *didCloseHandler) work() error {
 
 	return nil
 }
-
-// func (h *Handler) didClose(ctx context.Context, req *jsonrpc2.Request) handleFuncer {
-// 	var params DidCloseTextDocumentParams
-// 	if err := json.Unmarshal(*req.Params, &params); err != nil {
-// 		return noopHandleFuncer
-// 	}
-
-// 	uri := string(params.TextDocument.URI)
-// 	fpath := strings.TrimPrefix(uri, "file://")
-
-// 	_, ok := h.openedFiles[fpath]
-// 	if !ok {
-// 		h.log.Warnf("File %s is not opened\n", fpath)
-// 		return noopHandleFuncer
-// 	}
-
-// 	h.log.Debugf("File %s is open...\n", fpath)
-// 	delete(h.openedFiles, fpath)
-
-// 	astFile, err := parser.ParseFile(h.workspace.Fset, fpath, nil, 0)
-// 	if err != nil {
-// 		h.log.Errorf("Failed to parse file as provided by didOpen: %s\n", err.Error())
-// 	}
-
-// 	h.workspace.Files[fpath] = astFile
-
-// 	h.log.Debugf("File %s is closed\n", fpath)
-// 	return noopHandleFuncer
-// }
