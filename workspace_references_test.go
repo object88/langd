2934e77750d305cc2976ec2bd39dbf0c110fe97a
object88 <object88@gmail.com>
2018-02-15 20:01:19 -0700

    Implement tests

--- workspace_references_test.go
+++ workspace_references_test.go
@@ -7,15 +7,105 @@ import (
 )
 
 func Test_Workspace_References_Local_Const(t *testing.T) {
-	t.SkipNow()
+	src1 := `package foo
+	const fooVal = 0
+	func IncFoo() int {
+		return fooVal
+	}`
+
+	packages := map[string]map[string]string{
+		"foo": map[string]string{
+			"foo.go": src1,
+		},
+	}
+
+	w := workspaceSetup(t, "/go/src/foo", packages, false)
+
+	startPosition := &token.Position{
+		Filename: "/go/src/foo/foo.go",
+		Line:     4,
+		Column:   10,
+	}
+	referencePositions := []*token.Position{
+		&token.Position{
+			Filename: "/go/src/foo/foo.go",
+			Line:     2,
+			Column:   8,
+		},
+		startPosition,
+	}
+	testReferences(t, w, startPosition, referencePositions)
 }
 
 func Test_Workspace_References_Package_Const(t *testing.T) {
-	t.SkipNow()
+	src1 := `package foo
+	const fooVal = 0`
+
+	src2 := `package foo
+	func IncFoo() int {
+		return fooVal
+	}`
+
+	packages := map[string]map[string]string{
+		"foo": map[string]string{
+			"foo1.go": src1,
+			"foo2.go": src2,
+		},
+	}
+
+	w := workspaceSetup(t, "/go/src/foo", packages, false)
+
+	startPosition := &token.Position{
+		Filename: "/go/src/foo/foo2.go",
+		Line:     3,
+		Column:   10,
+	}
+	referencePositions := []*token.Position{
+		&token.Position{
+			Filename: "/go/src/foo/foo1.go",
+			Line:     2,
+			Column:   8,
+		},
+		startPosition,
+	}
+	testReferences(t, w, startPosition, referencePositions)
 }
 
 func Test_Workspace_References_Imported_Const(t *testing.T) {
-	t.SkipNow()
+	src1 := `package foo
+	const FooVal = 0`
+
+	src2 := `package bar
+	import "../foo"
+	func IncFoo() int {
+		return foo.FooVal
+	}`
+
+	packages := map[string]map[string]string{
+		"foo": map[string]string{
+			"foo.go": src1,
+		},
+		"bar": map[string]string{
+			"bar.go": src2,
+		},
+	}
+
+	w := workspaceSetup(t, "/go/src/bar", packages, false)
+
+	startPosition := &token.Position{
+		Filename: "/go/src/bar/bar.go",
+		Line:     4,
+		Column:   14,
+	}
+	referencePositions := []*token.Position{
+		&token.Position{
+			Filename: "/go/src/foo/foo.go",
+			Line:     2,
+			Column:   8,
+		},
+		startPosition,
+	}
+	testReferences(t, w, startPosition, referencePositions)
 }
 
 func Test_Workspace_References_Local_Var(t *testing.T) {
@@ -244,15 +334,112 @@ func Test_Workspace_References_Imported_Struct(t *testing.T) {
 }
 
 func Test_Workspace_References_Local_Interface(t *testing.T) {
-	t.SkipNow()
+	src1 := `package foo
+	type fooIface interface {
+		myNumber() int
+	}
+	func Do() int {
+		var f fooIface
+		return f.myNumber()
+	}`
+
+	packages := map[string]map[string]string{
+		"foo": map[string]string{
+			"foo.go": src1,
+		},
+	}
+
+	w := workspaceSetup(t, "/go/src/foo", packages, false)
+
+	startPosition := &token.Position{
+		Filename: "/go/src/foo/foo.go",
+		Line:     6,
+		Column:   9,
+	}
+	referencePositions := []*token.Position{
+		&token.Position{
+			Filename: "/go/src/foo/foo.go",
+			Line:     2,
+			Column:   7,
+		},
+		startPosition,
+	}
+	testReferences(t, w, startPosition, referencePositions)
 }
 
 func Test_Workspace_References_Package_Interface(t *testing.T) {
-	t.SkipNow()
+	src1 := `package foo
+	type fooIface interface {
+		myNumber() int
+	}`
+	src2 := `package foo
+	func Do() int {
+		var f fooIface
+		return f.myNumber()
+	}`
+
+	packages := map[string]map[string]string{
+		"foo": map[string]string{
+			"foo1.go": src1,
+			"foo2.go": src2,
+		},
+	}
+
+	w := workspaceSetup(t, "/go/src/foo", packages, false)
+
+	startPosition := &token.Position{
+		Filename: "/go/src/foo/foo2.go",
+		Line:     3,
+		Column:   9,
+	}
+	referencePositions := []*token.Position{
+		&token.Position{
+			Filename: "/go/src/foo/foo1.go",
+			Line:     2,
+			Column:   7,
+		},
+		startPosition,
+	}
+	testReferences(t, w, startPosition, referencePositions)
 }
 
 func Test_Workspace_References_Imported_Interface(t *testing.T) {
-	t.SkipNow()
+	src1 := `package foo
+	type FooIface interface {
+		MyNumber() int
+	}`
+	src2 := `package bar
+	import "../foo"
+	func Do() int {
+		var f foo.FooIface
+		return f.MyNumber()
+	}`
+
+	packages := map[string]map[string]string{
+		"foo": map[string]string{
+			"foo.go": src1,
+		},
+		"bar": map[string]string{
+			"bar.go": src2,
+		},
+	}
+
+	w := workspaceSetup(t, "/go/src/bar", packages, false)
+
+	startPosition := &token.Position{
+		Filename: "/go/src/bar/bar.go",
+		Line:     4,
+		Column:   13,
+	}
+	referencePositions := []*token.Position{
+		&token.Position{
+			Filename: "/go/src/foo/foo.go",
+			Line:     2,
+			Column:   7,
+		},
+		startPosition,
+	}
+	testReferences(t, w, startPosition, referencePositions)
 }
 
 func Test_Workspace_References_Local_Func(t *testing.T) {
@@ -398,15 +585,130 @@ func Test_Workspace_References_Local_Selector_Field(t *testing.T) {
 }
 
 func Test_Workspace_References_Package_Selector_Field(t *testing.T) {
-	t.SkipNow()
+	src1 := `package foo
+	type fooStruct struct {
+		a int
+	}`
+	src2 := `package foo
+	func Do() int {
+		f := &fooStruct{}
+		return f.a
+	}`
+
+	packages := map[string]map[string]string{
+		"foo": map[string]string{
+			"foo1.go": src1,
+			"foo2.go": src2,
+		},
+	}
+
+	w := workspaceSetup(t, "/go/src/foo", packages, false)
+
+	startPosition := &token.Position{
+		Filename: "/go/src/foo/foo2.go",
+		Line:     4,
+		Column:   12,
+	}
+	referencePositions := []*token.Position{
+		&token.Position{
+			Filename: "/go/src/foo/foo1.go",
+			Line:     3,
+			Column:   3,
+		},
+		startPosition,
+	}
+	testReferences(t, w, startPosition, referencePositions)
 }
 
 func Test_Workspace_References_Imported_Selector_Field(t *testing.T) {
-	t.SkipNow()
+	src1 := `package foo
+	type FooStruct struct {
+		A int
+	}`
+	src2 := `package bar
+	import "../foo"
+	func Do() int {
+		f := &foo.FooStruct{}
+		return f.A
+	}`
+
+	packages := map[string]map[string]string{
+		"foo": map[string]string{
+			"foo.go": src1,
+		},
+		"bar": map[string]string{
+			"bar.go": src2,
+		},
+	}
+
+	w := workspaceSetup(t, "/go/src/bar", packages, false)
+
+	startPosition := &token.Position{
+		Filename: "/go/src/bar/bar.go",
+		Line:     5,
+		Column:   12,
+	}
+	referencePositions := []*token.Position{
+		&token.Position{
+			Filename: "/go/src/foo/foo.go",
+			Line:     3,
+			Column:   3,
+		},
+		startPosition,
+	}
+	testReferences(t, w, startPosition, referencePositions)
 }
 
 func Test_Workspace_References_Indirect_Imported_Selector_Field(t *testing.T) {
-	t.SkipNow()
+	src1 := `package foo
+	type FooStruct struct {
+		A int
+	}`
+	src2 := `package bar
+	import "../foo"
+	type BarStruct struct {
+		F *foo.FooStruct
+	}
+	func NewBarStruct() *BarStruct {
+		return &BarStruct {
+			F: &foo.FooStruct{}
+		}
+	}`
+	src3 := `package baz
+	import "../bar"
+	func Do() int {
+		b := bar.NewBarStruct()
+		return b.F.A
+	}`
+
+	packages := map[string]map[string]string{
+		"foo": map[string]string{
+			"foo.go": src1,
+		},
+		"bar": map[string]string{
+			"bar.go": src2,
+		},
+		"baz": map[string]string{
+			"baz.go": src3,
+		},
+	}
+
+	w := workspaceSetup(t, "/go/src/baz", packages, false)
+
+	startPosition := &token.Position{
+		Filename: "/go/src/baz/baz.go",
+		Line:     5,
+		Column:   14,
+	}
+	referencePositions := []*token.Position{
+		&token.Position{
+			Filename: "/go/src/foo/foo.go",
+			Line:     3,
+			Column:   3,
+		},
+		startPosition,
+	}
+	testReferences(t, w, startPosition, referencePositions)
 }
 
 func Test_Workspace_References_Local_Selector_Method(t *testing.T) {
