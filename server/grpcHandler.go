757a300eccd48ea74b8de2e3774abe0410c16f16
object88 <object88@gmail.com>
2018-02-25 21:37:53 -0700

    Cleanup

--- server/grpcHandler.go
+++ server/grpcHandler.go
@@ -27,22 +27,15 @@ func (g *GrpcHandler) grpcService() {
 // Load returns the CPU and memory load
 func (g *GrpcHandler) Load(_ context.Context, _ *proto.EmptyRequest) (*proto.LoadReply, error) {
 	load := &proto.LoadReply{
-		CpuLoad:    g.srv.load.CPU(),    // float32(g.pc.Percent),
-		MemoryLoad: g.srv.load.Memory(), // g.pm.Resident,
+		CpuLoad:    g.srv.load.CPU(),
+		MemoryLoad: g.srv.load.Memory(),
 	}
 	return load, nil
 }
 
 // Shutdown stops the service process
 func (g *GrpcHandler) Shutdown(ctx context.Context, _ *proto.EmptyRequest) (*proto.EmptyReply, error) {
-	// fmt.Printf("Requesting stop on JSON server\n")
-	// g.SM.Shutdown(ctx)
-
-	fmt.Printf("Requesting stop on gRPC\n")
-	// g.S.GracefulStop()
-
 	g.srv.done <- true
-
 	return &proto.EmptyReply{}, nil
 }
 
